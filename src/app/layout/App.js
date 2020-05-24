876ee9ff5348387ad8f4750ca2e92d1cb8bcc2e8
Alex Greene <greene.alexanderj@gmail.com>
2020-05-24 17:27:36 -0400

    update file structure and addd a nav bar

---
+++ src/app/layout/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import NavBar from '../../features/nav/NavBar';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<NavBar />
+				<h1>Alex Greene's Portfolio Page</h1>
+				<p>Currently under construction</p>
+			</div>
+		);
+	}
+}
+
+export default App;
